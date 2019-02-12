03a483b4269291826ca5774515d050124fefab8f
tuantm <tuan.tm@citigo.com.vn>
2019-02-12 16:55:18 +0700

    Allow customize event background and text color

--- WRCalendarView/Events/WREvent.swift
+++ WRCalendarView/Events/WREvent.swift
@@ -11,11 +11,12 @@ import DateToolsSwift
 
 open class WREvent: TimePeriod {
     open var title: String = ""
+    open var backgroundColor: UIColor = #colorLiteral(red: 0.2078431373, green: 0.6941176471, blue: 0.9450980392, alpha: 0.09719071062)
+    open var textColor: UIColor = #colorLiteral(red: 0.1294117647, green: 0.4470588235, blue: 0.6117647059, alpha: 1)
     
     open class func make(date:Date, chunk: TimeChunk, title: String) -> WREvent {
         let event = WREvent(beginning: date, chunk: chunk)
         event.title = title
-        
         return event
     }
 }
--- WRCalendarView/Views/WREventCell.swift
+++ WRCalendarView/Views/WREventCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class WREventCell: UICollectionViewCell {
+    
     @IBOutlet weak var borderView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
 
@@ -22,37 +23,21 @@ class WREventCell: UICollectionViewCell {
         
         updateColors()
     }
-
-    override var isSelected: Bool {
-        didSet {
-            if isSelected && isSelected != oldValue {
-                UIView.animate(withDuration: TimeInterval(0.2), animations: { [unowned self] in
-                    self.transform = CGAffineTransform.init(scaleX: 1.025, y: 1.025)
-                    self.layer.shadowOpacity = 0.2
-                }, completion: { [unowned self] _ in
-                    self.transform = CGAffineTransform.identity
-                })
-            } else if isSelected {
-                layer.shadowOpacity = 0.2
-            } else {
-                layer.shadowOpacity = 0.0
-            }
-            updateColors()
-        }
-    }
     
     var event: WREvent? {
         didSet {
             if let event = event {
                 titleLabel.text = event.title
+                updateColors()
             }
         }
     }
     
     func updateColors() {
-        contentView.backgroundColor = backgroundColorHighlighted(isSelected)
-        borderView.backgroundColor = borderColor()
-        titleLabel.textColor = textColorHighlighted(isSelected)
+        guard let event = event else { return }
+        contentView.backgroundColor = event.backgroundColor
+        borderView.backgroundColor = event.backgroundColor.withAlphaComponent(1)
+        titleLabel.textColor = event.textColor
     }
     
     func backgroundColorHighlighted(_ selected: Bool) -> UIColor {
